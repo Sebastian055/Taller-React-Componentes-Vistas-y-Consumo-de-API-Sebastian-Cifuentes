b4a324eeafc1c8c1e58a96f68d8814ff2c070227
Sebastian055 <sebastian.cifuentes055@pascualbravo.edu.co>
2024-11-06 19:17:03 -0500

    Trabajo de Sebastian Cifuentes

---
+++ src/pages/PaginaAcercaDe.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function PaginaAcercaDe() {
+  return (
+    <div>
+      <h2>Pagina de Sebastian</h2>
+      <p>En esta pagina podremos ver el portafolio de Sebastian</p>
+    </div>
+  );
+}
+
+export default PaginaAcercaDe;
